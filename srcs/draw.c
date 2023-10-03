963d201b89ff22298794caa2d01e50d7351def73
samiya zubair <94432143+samiyazbr@users.noreply.github.com>
2023-10-03 22:02:27 +0400

    Add files via upload

---
+++ srcs/draw.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samiyazubair <samiyazubair@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 15:07:23 by samiyazubai       #+#    #+#             */
+/*   Updated: 2023/09/28 14:07:20 by samiyazubai      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+void mlx_place_pixel(int x, int y, int colour)
+{
+    // Calculate the memory address where the pixel should be placed
+    char *distance = data()->screen.address + \
+        (y * data()->screen.line_length + x * (data()->screen.bits_per_pixel / 8));
+
+    // Set the color value at the calculated memory address
+    *(unsigned int *)distance = colour;
+}
+
+
+void	draw_floor(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < SCREEN_WIDTH)
+	{
+		j = SCREEN_HEIGHT / 2;
+		while (j < SCREEN_HEIGHT)
+		{
+			// Place a pixel at the current (i, j) coordinates on the screen using the floor color
+			mlx_place_pixel(i, j, data()->floor);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	draw_ceiling(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < SCREEN_WIDTH)
+	{
+		j = 0;
+		while (j < SCREEN_HEIGHT / 2)
+		{
+			// Place a pixel at the current (i, j) coordinates on the screen using the ceiling color
+			mlx_place_pixel(i, j, data()->ceiling);
+			j++;
+		}
+		i++;
+	}
+}
---
+++ srcs/key.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samiyazubair <samiyazubair@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 14:03:19 by samiyazubai       #+#    #+#             */
+/*   Updated: 2023/09/27 18:43:44 by samiyazubai      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+int	key_press(int keycode)
+{
+	if (keycode == KEY_ESC)
+	{
+		printf("You Enter the ESC Key. Good Bye!\n");
+		exit_game();
+	}
+	else if (keycode == KEY_W)
+		data()->key.W = 1;
+	else if (keycode == KEY_S)
+		data()->key.S = 1;
+	else if (keycode == KEY_A)
+		data()->key.A = 1;
+	else if (keycode == KEY_D)
+		data()->key.D = 1;
+	else if (keycode == KEY_LEFT)
+		data()->key.left_key = 1;
+	else if (keycode == KEY_RIGHT)
+		data()->key.right_key = 1;
+	return (0);
+}
+
+int	key_release(int keycode)
+{
+	if (keycode == KEY_W)
+		data()->key.W = 0;
+	else if (keycode == KEY_S)
+		data()->key.S = 0;
+	else if (keycode == KEY_A)
+		data()->key.A = 0;
+	else if (keycode == KEY_D)
+		data()->key.D = 0;
+	else if (keycode == KEY_LEFT)
+		data()->key.left_key = 0;
+	else if (keycode == KEY_RIGHT)
+		data()->key.right_key = 0;
+	return (0);
+}
