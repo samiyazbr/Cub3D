594763ca681fe6ff97d24f45cc0c6e57b4585928
samiya zubair <94432143+samiyazbr@users.noreply.github.com>
2023-10-04 18:04:40 +0400

    Add files via upload

---
+++ srcs/main.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samiyazubair <samiyazubair@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 23:17:48 by hkunnam-          #+#    #+#             */
+/*   Updated: 2023/10/03 19:51:24 by samiyazubai      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+/*
+	This function lets us access the static variable type t_data,
+	the static variable retains its value between function calls,
+	we return a pointer to data as data is static, the memory 
+	location is fixed and can be accessed after the function
+	finished executing. it helps to	maintain state across 
+	different parts of the program and provide a global variable
+	like behaviour.
+*/
+t_data	*data(void)
+{
+	static t_data	data;
+
+	return (&data);
+}
+
+int	exit_game(void)
+{
+	printf("Exit game!\n");
+	mlx_destroy_window(data()->mlx, data()->win);
+	exit(0);
+	return (0);
+}
+
+int	begin_game(void)
+{
+	void	*image;
+	char	*data_address;
+
+	// Create a new image with MiniLibX with specified dimensions
+	image = mlx_new_image(data()->mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	// Store the image pointer in a data structure
+	data()->screen.pointer_to_image = image;
+
+	// Retrieve the image properties and its address using mlx function
+	data_address = mlx_get_data_address(data()->screen.pointer_to_image,
+		&data()->screen.bits_per_pixel, &data()->screen.line_length,
+		&data()->screen.endian);
+
+	// Store the image address in a data structure
+	data()->screen.address = data_address;
+
+	// Draw the floor of the game environment
+	draw_floor();
+
+	// Draw the ceiling of the game environment
+	draw_ceiling();
+
+	raycasting();
+
+	//player_movement
+	player_movement();
+
+	// Set the frame time to 16 milliseconds (ms) per frame, equivalent to 60 frames per second (FPS).
+	data()->player.frame_time = 16 / 1000.0;
+
+	// set move speed and rotate speed based on the frame time
+	data()->player.move_speed = data()->player.frame_time * 5.0;
+	data()->player.rotate_speed = data()->player.frame_time * 3.0;
+
+	// Display the image on the application window
+	// 0 , 0 - These are the X and Y coordinates within the window where the top-left corner of the image will be positioned
+	mlx_put_image_to_window(data()->mlx, data()->win,
+		data()->screen.pointer_to_image, 0, 0);
+
+	// Destroy the image to prevent memory leaks
+	mlx_destroy_image(data()->mlx, data()->screen.pointer_to_image);
+
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\nPlease Select The Map File");
+		return (1);
+	}
+	parse_cub_file(argv[1]);
+	//for(int i = 0; i < ft_array_length(data()->xpm);i++)
+	//	printf("%s\n",data()->xpm[i]); //this is causing a sig fault in init_textures()
+	//for(int i = 0; i < ft_array_length(data()->rgb);i++)
+		//printf("%s\n",data()->rgb[i]);
+	//for(int i = 0; i < ft_array_length(data()->map);i++)
+	//	printf("%s\n",data()->map[i]);
+	init();
+	mlx_loop_hook(data()->mlx, begin_game, NULL);
+	mlx_hook(data()->win, X_EVENT_KEY_PRESS, X_NO_EVENT_MASK, &key_press, NULL);
+	mlx_hook(data()->win, X_EVENT_KEY_RELEASE, X_NO_EVENT_MASK, &key_release, NULL);
+	mlx_hook(data()->win, X_EVENT_DESTROY_NOTIFY, X_NO_EVENT_MASK,
+		&exit_game, NULL);
+	mlx_loop(data()->mlx);
+	return (0);
+}
---
+++ srcs/raycasting.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samiyazubair <samiyazubair@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 22:24:55 by samiyazubai       #+#    #+#             */
+/*   Updated: 2023/10/04 18:03:17 by samiyazubai      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+// This function calculates the position and direction of a ray based on the given 'x' value.
+void find_rayposition_and_raydirection(int x)
+{
+    // Calculate the normalized camera_x coordinate based on the screen width.
+    // This value will range from -1 (leftmost) to 1 (rightmost) on the screen.
+    data()->player.camera_x = 2 * x / (double) SCREEN_WIDTH - 1;
+
+    // Calculate the x component of the ray direction by adding the camera's direction_x
+    // to the camera's plane_x, scaled by the camera_x coordinate.
+    data()->player.raydirection_x = data()->player.direction_x + \
+                            data()->player.plane_x * data()->player.camera_x;
+
+    // Calculate the y component of the ray direction by adding the camera's direction_y
+    // to the camera's plane_y, scaled by the camera_x coordinate.
+    data()->player.raydirection_y = data()->player.direction_y + \
+                            data()->player.plane_y * data()->player.camera_x;
+}
+
+// This function sets the position of a wall in the game world and initializes the 'hit' variable.
+void set_wall_position(void)
+{
+    // Convert the double 'position_x' to an integer to determine the box's map grid X coordinate.
+    data()->player.map_x = (int) data()->player.position_x;
+
+    // Convert the double 'position_y' to an integer to determine the box's map grid Y coordinate.
+    data()->player.map_y = (int) data()->player.position_y;
+
+    // Initialize the 'hit' variable to 0, indicating that the box has not been hit.
+    data()->player.hit = 0;
+}
+
+// This function calculates the delta distance in the x and y directions for the raycasting algorithm.
+void set_delta_distance(void)
+{
+    // Check if the ray's x-direction component is zero.
+    if (data()->player.raydirection_x == 0)
+    {
+        // If the ray is parallel to the y-axis, set delta_distance_x to a very large value.
+        data()->player.delta_distance_x = 1e20;
+    }
+    else
+    {
+        // Calculate delta_distance_x as the absolute value of 1 divided by the ray's x-direction component.
+		//fabs() gets the absolute value
+        data()->player.delta_distance_x = fabs(1 / data()->player.raydirection_x);
+    }
+
+    // Check if the ray's y-direction component is zero.
+    if (data()->player.raydirection_y == 0)
+    {
+        // If the ray is parallel to the x-axis, set delta_distance_y to a very large value.
+        data()->player.delta_distance_y = 1e20;
+    }
+    else
+    {
+        // Calculate delta_distance_y as the absolute value of 1 divided by the ray's y-direction component.
+        data()->player.delta_distance_y = fabs(1 / data()->player.raydirection_y);
+    }
+}
+
+void find_x_movement_and_x_intersection_distance(void)
+{
+    // Check if the ray direction in the x-axis is negative
+    if (data()->player.raydirection_x < 0)
+    {
+        // If it's negative, set x_movement to -1 (moving to the left)
+        data()->player.x_movement = -1;
+        
+        // Calculate the x_intersection_distance using the formula:
+        // (position_x - map_x) * delta_distance_x
+        data()->player.x_intersection_distance = (data()->player.position_x
+                                                - data()->player.map_x)
+                                                * data()->player.delta_distance_x;
+    }
+    else
+    {
+        // If the ray direction in the x-axis is non-negative, set x_movement to 1 (moving to the right)
+        data()->player.x_movement = 1;
+        
+        // Calculate the x_intersection_distance using the formula:
+        // (map_x + 1.0 - position_x) * delta_distance_x
+        data()->player.x_intersection_distance = (data()->player.map_x + 1.0
+                                                - data()->player.position_x)
+                                                * data()->player.delta_distance_x;
+    }
+}
+
+void    raycasting(void)
+{
+	int	x;
+
+	x = 0;
+	while (x < SCREEN_WIDTH)
+	{
+		find_rayposition_and_raydirection(x);
+		set_wall_position();
+		set_delta_distance();
+		find_x_movement_and_x_intersection_distance();
+		find_y_movement_and_y_intersection_distance();
+		dda();
+        x++;
+    }
+}
